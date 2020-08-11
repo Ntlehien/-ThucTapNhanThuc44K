549887a5181be8613d93e06095d77615c7318a13
tuyen0601 <69445012+tuyen0601@users.noreply.github.com>
2020-08-11 22:19:25 +0700

    Update 3

--- 04/SQL Script/QUANLY_CTV.sql
+++ 04/SQL Script/QUANLY_CTV.sql
@@ -13,39 +13,38 @@ Add Constraint FK_LOAI_SAN_PHAM Foreign Key (MaLoaiSP) references LOAI_SAN_PHAM 
 select * from CONG_TAC_VIEN
 select * from LOAI_SAN_PHAM
 select * from HOA_DON
---
 Insert into CONG_TAC_VIEN
 Values ('CTV001','Mai Phong','Da Nang','1'),
        ('CTV002','Tran Tu','Da Nang','1'),
-	   ('a3','phong nam','ho chi minh','0'),
-	   ('a4','pham huong','ho chi minh','1'),
-	   ('a5','nha uyen','ha noi','1'),
-	   ('a6','tran mai','ho chi minh','0'),
-	   ('a7','bich thuy','kon tum','1'),
-	   ('a8','phu truong','dak lak','1'),
-	   ('a9','mai thuy','da lat','0'),
-	   ('a10','phuong hai','ho chi minh','0')
+	   ('CTV003','Phong Nam','Ho Chi Minh','0'),
+	   ('CTV004','Pham Huong','Ho Chi Minh','1'),
+	   ('CTV005','Nha Uyen','Ha Noi','1'),
+	   ('CTV006','Tran Mai','Ho Chi Minh','0'),
+	   ('CTV007','Bich Thuy','Kon Tum','1'),
+	   ('CTV008','Phu Truong','Dak Lak','1'),
+	   ('CTV009','Mai Thuy','Da Lat','0'),
+	   ('CTVT0010','Phuong Hai','Ho Chi Minh','0')
 --
 Insert into LOAI_SAN_PHAM
-Values ('SP001','Nuoc suoi','200','VND'),
-       ('SP002','Nuoc co gas','300','VND'),
-	   ('SP003','Nuoc trai cay','300','VND'),
-	   ('04','banh ngot','500','dolar'),
-	   ('05','banh man','600','dolar'),
-	   ('06','vai thong thuong','300','dolar'),
-	   ('07','vai thong dung','500','dolar'),
-	   ('08','vai xuat khau','900','dolar'),
-	   ('09','muc trung','100','vnd'),
-	   ('010','muc tot','200','vnd')
+Values ('001','Nuoc suoi','200','VND'),
+       ('002','Nuoc co gas','300','VND'),
+	   ('003','Nuoc trai cay','300000','VND'),
+	   ('004','Banh ngot','500000','VND'),
+	   ('005','Banh man','600000','VND'),
+	   ('006','Vai thong thuong','300000','VND'),
+	   ('007','Vai thong dung','500000','VND'),
+	   ('008','Vai xuat khau','900000','VND'),
+	   ('009','Muc trung','100000','VND'),
+	   ('0010','Muc tot','200000','VND')
 --
 insert into HOA_DON 
-values ('01','a1','2','100'),
-       ('02','a2','3','900'),
-	   ('03','a3','2','600'),
-	   ('04','a4','2','1000'),
-	   ('05','a5','6','1200'),
-	   ('07','a7','8','2100'),
-	   ('08','a8','2','1000'),
-	   ('09','a9','1','100'),
-	   ('010 ','a10','1','100')
-	
+values ('001','CTV001','2','100000'),
+       ('002','CTV002','3','900000'),
+	   ('003','CTV003','2','600000'),
+	   ('004','CTV004','2','1000000'),
+	   ('005','CTV005','6','1200000'),
+	   ('006','CTV006','8','2100000'),
+	   ('007','CTV007','2','1000000'),
+	   ('008','CTV008','1','100000'),
+	   ('009','CTV009','1','100000'),
+	   ('0010','CTV0010','1','200000')
