a1b27ecf0d705ab111d169c427caee05599b11af
CHƯƠNG <69464481+NgocChuong9012000@users.noreply.github.com>
2020-08-12 18:47:28 +0700

    NgocChuong Update SQL

--- 06/SQL Script/Nhom_6.sql
+++ 06/SQL Script/Nhom_6.sql
@@ -1,14 +1,17 @@
 ﻿create database KD_GAMEONLINE
+GO
 use KD_GAMEONLINE
+GO
 --
 Create table MAY
 (
 	MaMay int not null,
-	MoTa nvarchar (100) default N'kich co man hinh, cauhinh,...' not null,
-	TrangThai varchar (10) default N'Hong,Dang thue, san co' not null,
+	MoTa nvarchar (100) not null,
+	TrangThai varchar (10) not null,
 	DonGiaThue int
 	primary key (MaMay)
 )
+GO
 --
 create table KHACH
 ( 
@@ -16,7 +19,8 @@ create table KHACH
 	TenK nvarchar (100) not null,
 	DiaChi nvarchar (100) not null
 	primary key (SoDT)
-	)
+)
+GO
 --
 Create table THUE
 (
@@ -30,6 +34,7 @@ Create table THUE
 	foreign Key (MaMay) References MAY (MaMay),
 	foreign key (SoDT) References KHACH (SoDT)
 )
+GO
 --
 insert into MAY 
 values  ('001','corei3,15.6inch','hong',2500),
@@ -37,6 +42,7 @@ values  ('001','corei3,15.6inch','hong',2500),
 		('003','corei8,18.5inch','dangthue',2100),
 		('004','corei3,15inch','sanco',3000),
 		('005','corei5,15.6inch','hong',1700)
+GO
 select*from MAY
 --
 insert into KHACH
@@ -45,6 +51,7 @@ values  ('0866750241','Nguyễn Thị Thảo Nguyên','NguHanhSon'),
 		('0558228435','Nguyễn Phước Trung','NguHanhSon'),
 		('0866755469','Nguyễn Đăng Hưng','LienChieu'),
 		('0355960436','Lê Huỳnh Đan','HoaVang')
+GO
 select*from KHACH
 --
 insert into THUE (MaMay,SoDT,NgayVao,GioVao,NgayRa,GioRa)
@@ -53,6 +60,7 @@ values  ('003','0356778905','2019-10-20','15:22:31','2019-10-20','18:40:07'),
 		('004','0355960436','2019-04-16','08:25:08','2019-04-16','15:20:05'),
 		('001','0558228435','2019-07-18','23:30:00','2019-07-19','11:01:45'),
 		('005','0866750241','2019-03-15','08:22:00','2019-03-15','21:30:00')
+GO
 select*from THUE
 --
 
